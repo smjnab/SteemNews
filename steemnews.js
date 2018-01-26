4827f2f77149035131fa48f9f0d125513f9643e1
smjnab <web@jensnilsson.com>
2018-01-26 10:34:01 +0100

    Update steemnews.js
    - Using showdown to convert markdown to html, saves about 70 lines.
    - Add failover if main steemit.com endpoint down, tries 3 different servers.
    - News/Blog tag can now be any tag, before only the main tag was possible.
    - Added buttons to select if you want to show both tags or only one of them.

--- steemnews.js
+++ steemnews.js
@@ -3,7 +3,10 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 /// DSteem client for Steemit.com
-const client = new dsteem.Client("https://api.steemit.com");
+var client = new dsteem.Client("https://api.steemit.com", { timeout: 2000 });
+const secondServer = "https://api.steemitstage.com/"; // If api.steemit.com does not respond, try this server.
+const thirdServer = "https://gtg.steem.house:8090"; // If api.steemit.com does not respond, try this server.
+const fourthServer = "https://rpc.steemliberator.com/"; // If api.steemit.com does not respond, try this server.
 
 const pagePostFetchLimit = 25; /// Fetch 25 posts from blog. Fecthing 25 as not all might be news/blog posts.
 const pagePostCountLimit = 5; /// How many posts for each news page.
@@ -13,6 +16,12 @@ const tagBlog = "Additional tag to use as news."; /// I have a blog tag, this se
 
 var lastPost = "";  /// The perm link of last post. Used to load more news if wanted.
 var loadNewsOnScroll = false;   ///Makes sure only one load new news triggers at last post.
+
+var postType = "";  ///Show blog & post or only one of them.
+var failCount = 0;  /// Switch to secondary and third server on fails.
+
+/// Fetch news on first load.
+GetSteemDiscussions("");
 
 /// When reaching last news post, load additional news.
 $(window).scroll(function () {
@@ -25,8 +34,15 @@ $(window).scroll(function () {
     }
 });
 
-/// Fetch news on first load.
-GetSteemDiscussions();
+/// Click on a radio button to select post types to show.
+$(document).on("click", "#radioNewsBoth,#radioNewsNews,#radioNewsBlog", function (event) {
+
+    postType = event.target.id; /// Use the id of button to set different post types.
+
+    lastPost = "";  //Reset last post as changing type of posts to show.
+
+    GetSteemDiscussions();  /// Update post listing.
+});
 
 /// Big pile of function for everything steem.
 function GetSteemDiscussions() {
@@ -39,7 +55,6 @@ function GetSteemDiscussions() {
 
     /// Get blog posts from Steemit
     client.database.getDiscussions("blog", discussionQuery).then(function (discussions) {
-
         /// Style of the date of each post.
         var dateStyle = { year: "numeric", month: "long", day: "numeric" };
         var dateTimeFormat = new Intl.DateTimeFormat("en-EN", dateStyle);
@@ -47,20 +62,31 @@ function GetSteemDiscussions() {
         /// Count number of posts, used to limit news posts as fetching more incase blog has resteems etc.
         var postCount = 0;
 
-        /// Remove loading when fetched news the first time.
-        if (lastPost == "") $("#main .inner").html("<h1>Latest News &amp; Blog Posts</h1>");
+        /// Remove loading.
+        if (lastPost == "") $("#main .innerNews").html("");
 
         /// Go over fetched discussions and take the posts that matches what we want.
         var blogPosts = [];
 
+        /// Always use only news post type if no set.
+        if (postType == "") postType = "radioNewsNews";
+
         for (i = 0; i < discussions.length; i++) {
             blogPost = discussions[i];  ///Each blog post.
-            metaTags = JSON.parse(discussions[i].json_metadata).tags; /// Each additional tag
-            tagFound = false;
+            var metaTags = JSON.parse(discussions[i].json_metadata).tags; /// Each additional tag
+            var tagFound = false;
 
             /// Check main and additional tags for the tags we want.
-            if (blogPost.category == tagNews || blogPost.category == tagBlog ||
-                metaTags.indexOf(tagNews) >= 0 || metaTags.indexOf(tagBlog) >= 0)
+            if (postType == "radioNewsBoth" && (blogPost.category == tagNews || blogPost.category == tagBlog ||
+                metaTags.indexOf(tagNews) >= 0 || metaTags.indexOf(tagBlog) >= 0))
+                tagFound = true;
+
+            /// Check main and additional tags for news only.
+            else if (postType == "radioNewsNews" && (blogPost.category == tagNews || metaTags.indexOf(tagNews) >= 0))
+                tagFound = true;
+
+            /// Check main and additional tags for blog only.
+            else if (postType == "radioNewsBlog" && (blogPost.category == tagBlog || metaTags.indexOf(tagBlog) >= 0))
                 tagFound = true;
 
             /// Add post when tag found, new post and author correct
@@ -70,7 +96,6 @@ function GetSteemDiscussions() {
 
         /// For each post, format the text properly.
         blogPosts.forEach(blogPost => {
-
             if (postCount < pagePostCountLimit) {
 
                 /// Change the date format and remove time.
@@ -79,70 +104,20 @@ function GetSteemDiscussions() {
                 /// Check body string
                 var bodyString = DOMPurify.sanitize(blogPost.body, { SAFE_FOR_JQUERY: true });
 
-                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                /// Markup to HTML. (Stupid thing as obviously there are libs for this... did not think of it before doing it. Doh!)
+                /// Convert markdown to html
+                var converter = new showdown.Converter();
 
-                /// Convert new lines to html line breaks.
-                bodyString = bodyString.replace(/\n/ig, "<br />");
+                converter.setFlavor("github");
+                converter.setOption("simplifiedAutoLink", "true");
+                converter.setOption("simpleLineBreaks", "true");
+                converter.setOption("headerLevelStart", "2");
+                converter.setOption("ghMentions", "true");
+                converter.setOption("ghMentionsLink", "https://steemit.com/@{u}");
 
-                /// Remove weird "hr" from steepshot.
-                bodyString = bodyString.replace(/[\- ]{10,40}/ig, "");
+                bodyString = converter.makeHtml(bodyString);
 
-                /// Remove too many white lines from steepshot.
-                bodyString = bodyString.replace(/<br \/><br \/><br \/><br \/>/ig, "<br \/>");
-
-                /// Remove any center tags.
-                bodyString = bodyString.replace(/<center>/ig, "");
-                bodyString = bodyString.replace(/<\/center > /ig, "");
-
-                /// Remove <hr>
+                /// Remove any <hr> tags.
                 bodyString = bodyString.replace(/<hr>/ig, "");
-
-                /// Steepshot specifics
-                if (bodyString.indexOf("Created with Steepshot") >= 0) {
-
-                    /// Remove created with steepshot section.
-                    bodyString = bodyString.replace(
-                        /!\[[a-zA-Z0-9_\-./]*\.(jpg|jpeg|png|svg|gif)\]\(https?:\/\/[a-zA-Z0-9_\-:;./!?&=+~@ <>)([\]]*Created with Steepshot[a-zA-Z0-9_\-:;./!?&=+~@ <>)([\]]*/ig,
-                        ''
-                    );
-
-                    /// Make steepshot url for image into <img tag.
-                    bodyString = bodyString.replace(
-                        /(https?:\/\/steepshot[a-zA-Z0-9_\-:;./!?&=+~@]*\.(jpg|jpeg|png|svg|gif))/ig,
-                        '<span class="image tweak"><img src="$1"  alt="" /></span>'
-                    );
-                }
-
-                /// Add class to any <img tags.
-                bodyString = bodyString.replace(
-                    /<img src=('|")([a-zA-Z0-9_\-:;./!?&=+~ ]*)('|")>/ig,
-                    '<span class="image tweak"><img src="$2"  alt="" /></span>'
-                );
-
-                /// Make Steemit image code into site specific tags.
-                bodyString = bodyString.replace(
-                    /\!\[[a-zA-Z0-9_\-./]*\.(jpg|jpeg|png|svg|gif)\]\((https?:\/\/[a-zA-Z0-9_\-:;./!?&=+~@]*)\)/ig,
-                    '<span class="image tweak"><img src="$2"  alt="" /></span>'
-                );
-
-                /// Make Steemit url code into site specific tags.
-                bodyString = bodyString.replace(
-                    /\[([a-zA-Z -]*)\]\((https?:\/\/[a-zA-Z0-9_\-:;./!?&=+~@]*)\)/ig,
-                    '<a href="$2" target="_blank">$1</a>'
-                );
-
-                /// Make urls written as text into url tags.
-                bodyString = bodyString.replace(
-                    /([^"'])(https?:\/\/[a-zA-Z0-9_\-:;./!?&=+~@]*)([^"']|$)/ig,
-                    '$1<a href="$2" target="_blank">$2</a>$3'
-                );
-
-                /// Make @steem_username into links to steemit.com user blog
-                bodyString = bodyString.replace(
-                    /([^/])(@[a-z0-9-]{3,16})/ig,
-                    '$1<a href="https://steemit.com/$2" target="_blank">$2</a>'
-                );
 
                 /// Make #taginto links to steemit.com tags
                 bodyString = bodyString.replace(
@@ -150,25 +125,16 @@ function GetSteemDiscussions() {
                     '$1<a href="https://steemit.com/trending/$2" target="_blank">$2</a>'
                 );
 
-                /// Make ** into bold
+                /// Add class to any <img tags.
                 bodyString = bodyString.replace(
-                    /(\*\*)(.*)(\*\*)/ig,
-                    '<b>$2</b>'
+                    /<img src="([a-zA-Z0-9_\-:;./!?&=+~]*)" alt="([a-zA-Z0-9_\-:;./!?&=+~]*)" \/>/ig,
+                    '<span class="image tweak"><img src="$1"  alt="$2" /></span>'
                 );
-
-                /// Make #->##### into bold
-                bodyString = bodyString.replace(
-                    /(\#{1,5} )([a-zA-Z0-9_\-:;./!?&=+~ ]*)(<br \/>)/ig,
-                    '<b>$2</b>$3'
-                );
-
-                /// Remove too much white lines after span.
-                bodyString = bodyString.replace(/<\/span><br \/><br \/>/ig, "<\/span><br \/>");
-                bodyString = bodyString.replace(/<\/span><br \/><br \/><br \/>/ig, "<\/span><br \/>");
 
                 /// Indicate what type of post: news or blog.
+                var metaTags = JSON.parse(blogPost.json_metadata).tags;
                 var blogType;
-                if (blogPost.category == tagNews) blogType = "News"
+                if (blogPost.category == tagNews || metaTags.indexOf(tagNews) >= 0) blogType = "News"
                 else blogType = "Blog"
 
                 /// Check strings
@@ -176,12 +142,12 @@ function GetSteemDiscussions() {
                 var postURL = DOMPurify.sanitize(blogPost.url, { SAFE_FOR_JQUERY: true });
 
                 /// Append each news post to the main inner div.
-                $("#main .inner").append(
-                    `<ul class="alt"><li><b>${postTitle}</b></li>
-                            <li><sup><i>${blogType} published ${creationDate}</i></sup></li></ul>
-                            <p class="news">${bodyString}
+               $("#main .innerNews").append(
+                    `<div class="newsHead"><ul class="alt"><li><h2 class="h2m">${postTitle}</h2></li>
+                            <li><sup><i>${blogType} published ${creationDate}</i></sup></li></ul></div>
+                            ${bodyString}
                                 <p><i><a href="https://busy.org${postURL}" target="_blank">Comment on busy.org</a></i></p>
-                            </p><br />`
+                            <p><br /></p>`
                 );
 
                 postCount++;
@@ -191,5 +157,26 @@ function GetSteemDiscussions() {
         });
 
         loadNewsOnScroll = true; /// Allow checking if reaching end of news to trigger fetch of more.
+    }, function (error) {
+        if (error.message.toLowerCase().includes("network") && failCount < 3) {
+            /// Try again with second server.
+            if (failCount === 0) {
+                client = new dsteem.Client(secondServer, { timeout: 3000 });
+            }
+
+            /// Try again with third server.
+            else if (failCount === 1) {
+                client = new dsteem.Client(thirdServer, { timeout: 5000 });
+            }
+
+            /// Try again with fourth server.
+            else if (failCount === 2) {
+                client = new dsteem.Client(fourthServer, { timeout: 10000 });
+            }
+
+            GetSteemDiscussions();
+
+            failCount++;
+        }
     });
 }
